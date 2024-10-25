605b5d87d549547fd74820cc44a4af9c3067be97
Sugoi <timyrisaev@gmail.com>
2024-10-25 15:39:43 +0300

    new entity "comment", getAll api

---
+++ Mappers/CommentMappers.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnetApi.Dtos.Comment;
+using dotnetApi.Models;
+
+namespace dotnetApi.Mappers
+{
+    public static class CommentMappers
+    {
+        public static CommentDto ToCommentDto(this Comment commentModel)
+        {
+            return new CommentDto
+            {
+                Id = commentModel.Id,
+                Title = commentModel.Title,
+                Content = commentModel.Content,
+                CreateOn = commentModel.CreateOn,
+                StockId = commentModel.StockId
+            };
+        }
+    }
+}
