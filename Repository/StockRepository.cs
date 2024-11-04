f47b75da95f4c41a07031730730baa8102149457
Sugoi <timyrisaev@gmail.com>
2024-11-04 14:24:57 +0200

    add pagination for stocks

--- Repository/StockRepository.cs
+++ Repository/StockRepository.cs
@@ -38,7 +38,9 @@ namespace dotnetApi.Repository
                 }
             }
 
-            return await stocks.ToListAsync();
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
+
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
         }
         public async Task<Stock?> GetByIdAsync(int id)
         {
